2a310913a076eb3977e98c47b7adbf30a327a0c9
delimited0 <patrickding00@gmail.com>
2022-04-12 22:25:18 -0500

    burnin

--- R/ep_sampling.R
+++ R/ep_sampling.R
@@ -17,7 +17,7 @@ get_polytope_constraints <- function(lb, ub, A, mu, L) {
 #' @param J number of slices per iteration (recycling)
 #' @export
 rtmvn <- function(n, mu, Sigma, lb, ub, A = NULL, method = "epess", initial = NULL,
-                  N = 1, J = 1, verbose = FALSE) {
+                  N = 1, J = 1, verbose = FALSE, burnin = 0) {
   
   L <- t(chol(Sigma))
   d <- length(mu)
@@ -36,9 +36,13 @@ rtmvn <- function(n, mu, Sigma, lb, ub, A = NULL, method = "epess", initial = NU
   pc <- get_polytope_constraints(lb, ub, A, mu, L)
   moments <- moments2(rep(0, d), diag(d), lb - A %*% mu, ub - A %*% mu, A %*% L)
   
-  std_samples = t( sample_epess(n, moments$mu, chol(moments$Sigma), 
+  total_samples = n + burnin
+  std_samples = t( sample_epess(total_samples, moments$mu, chol(moments$Sigma), 
                              pc$A, pc$b,
                              J, N, initial, verbose) )
+  
+  # discard burn in samples
+  std_samples = std_samples[(burnin+1):total_samples, ]
   
   # undo the approximate mean shift transformation
   std_samples = std_samples + matrix(rep(moments$mu, n), nrow = n, byrow = TRUE)
